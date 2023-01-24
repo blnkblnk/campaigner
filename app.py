ced7e15e401e910e88c1deb04bad721fdddcd6f6
Lucas Mattos <lukimattos@gmail.com>
2023-01-24 13:53:16 -0500

    table changing in size and consistent

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def index():
-    # generates a random number between 10 and 20
-    num_cells = random.randint(10, 20)
-    return render_template("index.html", num_cells=num_cells)
+    return render_template("index.html")
 
+num = 1
 
 @app.route("/generate_table")
 def generate_table():
+    global num
+    num += 1
+    num %= 15
     num_cells = random.randint(10, 20)
+    num_cells = num
     table_html = render_template("table.html", num_cells=num_cells)
     return table_html
